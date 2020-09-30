1911248193a4c99f088e96e75522cbf52b246bdf
DavidWells <hello@davidwells.io>
2020-09-30 16:11:04 -0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const transform = require('lodash.transform')
+const isEqual = require('lodash.isequal')
+const isArray = require('lodash.isarray')
+const isObject = require('lodash.isobject')
+
+/**
+ * Find difference between two objects
+ * @param  {object} origObj - Source object to compare newObj against
+ * @param  {object} newObj  - New object with potential changes
+ * @return {object} differences
+ */
+module.exports = function difference(origObj, newObj) {
+  function changes(newObj, origObj) {
+    let arrayIndexCounter = 0
+    return transform(newObj, function (result, value, key) {
+      if (!isEqual(value, origObj[key])) {
+        let resultKey = isArray(origObj) ? arrayIndexCounter++ : key
+        result[resultKey] = (isObject(value) && isObject(origObj[key])) ? changes(value, origObj[key]) : value
+      }
+    })
+  }
+  return changes(newObj, origObj)
+}
